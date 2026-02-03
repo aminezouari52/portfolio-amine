6cb9142d666870f2df0cd25619ba7fa21b5af59e
Amine Zouari <zouariamine52@gmail.com>
2026-02-03 10:13:34 +0100

    fix: update experience years in Hero section

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -20,10 +20,10 @@ export default function Hero() {
         Full Stack Web Engineer <br />
         <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
           <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent py-4 bg-gradient-to-r  from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-            <span className="">2+ years of experience</span>
+            <span className="">3+ years of experience</span>
           </div>
           <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
-            <span className="">2+ years of experience</span>
+            <span className="">3+ years of experience</span>
           </div>
         </div>
       </h2>
